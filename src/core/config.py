976029d1fa358f5531baea8d42969d2974a1edb2
Yury Bolshakov <juriii155@gmail.com>
2025-10-23 17:53:43 +0300

    added settings for SQLAlchemy

--- src/core/config.py
+++ src/core/config.py
@@ -2,10 +2,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
     )
 
+    @property
+    def database_url(self) -> str:
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-settings = Settings()
+
+settings = Settings()  # pyright: ignore[reportCallIssue]
