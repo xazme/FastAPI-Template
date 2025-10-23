0e3fe316a3b7c70fb943be6472f050163984dd4a
Yury Bolshakov <juriii155@gmail.com>
2025-10-23 18:10:25 +0300

    added new column for declarative base class for SQLAlchemy

--- src/database/base.py
+++ src/database/base.py
@@ -1,6 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
-    pass
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        unique=True,
+        nullable=False,
+        primary_key=True,
+        index=True,
+        default=uuid.uuid4,
+    )
