5b7d29d07a24ad51c6c1819163e4be5796bd6287
Yury Bolshakov <juriii155@gmail.com>
2025-10-25 11:50:21 +0300

    rewrite __init__ file for config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,1 +1,23 @@
-from .config import settings  # type:ignore[pylance fake allert]
+from typing import Union
+from .components import ComponentsConfig
+from .envs import DevelopmentConfig, ProductionConfig
+
+
+class ProductionSettings(ProductionConfig, ComponentsConfig): ...
+
+
+class DevelopmentSettings(DevelopmentConfig, ComponentsConfig): ...
+
+
+def get_settings() -> Union[ProductionSettings, DevelopmentSettings]:
+    env = ComponentsConfig().env
+    print(env)
+    if env == "development":
+        return DevelopmentSettings()
+    elif env == "production":
+        return ProductionSettings()
+    else:
+        raise ValueError(f"Unknown environment: {env}")
+
+
+settings: Union[ProductionSettings, DevelopmentSettings] = get_settings()
