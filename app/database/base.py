4de53fbfa10b9b72de64cebbf63ff2bd0b6cf081
Yury Bolshakov <juriii155@gmail.com>
2025-10-30 19:41:15 +0300

    rewrite declarative base class

--- app/database/base.py
+++ app/database/base.py
@@ -1,15 +1,34 @@
 import uuid
+from datetime import datetime
+from sqlalchemy import DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
+        type_=UUID(as_uuid=True),
         unique=True,
         nullable=False,
         primary_key=True,
         index=True,
         default=uuid.uuid4,
     )
+    created_at: Mapped[datetime] = mapped_column(
+        type_=DateTime(timezone=True),
+        nullable=False,
+        index=True,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        type_=DateTime(timezone=True),
+        nullable=False,
+        index=True,
+        onupdate=func.now(),
+        server_default=func.now(),
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls):
+        return cls.__name__.lower()
