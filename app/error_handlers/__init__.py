0d183473d1c16096740fd8026a0ab775bb1d590e
Yury Bolshakov <juriii155@gmail.com>
2025-10-30 19:42:44 +0300

    created __init__ file for error handlers

---
+++ app/error_handlers/__init__.py
@@ -0,0 +1,7 @@
+from .error_handlers import (
+    not_found_handler,
+    already_exists_handler,
+    validation_error_handler,
+)
+
+__all__ = ["not_found_handler", "already_exists_handler", "validation_error_handler"]
