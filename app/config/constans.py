11bc98802efc92c01f008c4bd6a8bc07e292f6b5
Yury Bolshakov <juriii155@gmail.com>
2025-11-05 12:57:35 +0300

    clean up code

--- app/config/constans.py
+++ app/config/constans.py
@@ -1,7 +1,4 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).parents[
-    2
-]  # спускаемся на 2 уровня ниже относительно этого файла
-APP_DIR = ROOT_DIR.joinpath("app")  # добавляем к пути папку исходников проекта
-ENV_FILE_PATH = ROOT_DIR.joinpath(".env")  # добавляем путь до env файла
+# path for .env file
+ENV_FILE_PATH = ROOT_DIR = Path(__file__).parents[2].joinpath(".env")
