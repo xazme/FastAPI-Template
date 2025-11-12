21700963b0e7fe0a0aa261e051d800925f8b5836
Yury Bolshakov <juriii155@gmail.com>
2025-11-12 21:41:57 +0300

    replace app lifespan to another file

--- app/server/server.py
+++ app/server/server.py
@@ -1,22 +1,9 @@
-from typing import AsyncGenerator
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.config import settings
-from app.database import db_helper
 from app.api import init_routers
 from app.middlewares import init_middlewares
 from app.exception_handlers import init_exception_handlers
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    try:
-        # await db_helper.create_tables()
-        yield
-        # await db_helper.drop_tables()
-        await db_helper.dispose()
-    except Exception:
-        raise
+from .lifespan import app_lifespan
 
 
 def create_app() -> FastAPI:
@@ -24,7 +11,7 @@ def create_app() -> FastAPI:
         title="Hide",
         description="Hide API",
         version="1.0.0",
-        lifespan=lifespan,
+        lifespan=app_lifespan,
         docs_url=settings.docs_url,
         redoc_url=settings.redoc_url,
     )
