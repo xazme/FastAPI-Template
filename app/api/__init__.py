4c74f83a068a53db5c9dcd64a7deb791413924d0
Yury Bolshakov <juriii155@gmail.com>
2025-11-12 21:37:49 +0300

    separate server components

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, APIRouter
 from .user.user_router import router as user_router
 
 router = APIRouter()
 router.include_router(router=user_router, prefix="/user", tags=["User"])
-__all__ = ["router"]
 
 
-def main(): ...
+def init_routers(app: FastAPI):
+    app.include_router(router=router)
+
+
+__all__ = ["init_routers"]
