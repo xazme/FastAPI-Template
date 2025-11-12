9dc67f82410f2dd94467f941fc391562d720e375
Yury Bolshakov <juriii155@gmail.com>
2025-11-12 19:19:12 +0300

    replaced && renamed db exceptions

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,10 @@
 from .base import Base
 from .database_helper import db_helper
+from .db_exceptions import ObjectNotFoundError, ObjectAlreadyExistsError
 
-__all__ = ["Base", "db_helper"]
+__all__ = [
+    "Base",
+    "db_helper",
+    "ObjectNotFoundError",
+    "ObjectAlreadyExistsError",
+]
