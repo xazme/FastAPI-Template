4394ebf0cb49611450429e662c82c2fcb043fb99
Yury Bolshakov <juriii155@gmail.com>
2026-01-26 20:27:54 +0300

    set new base level for logger + created __init__ file for this

--- app/core/logger/logger_config.py
+++ app/core/logger/logger_config.py
@@ -8,7 +8,7 @@ def init_logger() -> None:
         datefmt="%Y-%m-%d %H:%M:%S",
         format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
         style="%",
-        level=logging.DEBUG,
+        level=logging.INFO,
         encoding="utf-8",
         force=True,
     )
