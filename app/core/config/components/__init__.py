9cbb808626b6f66535c2b4a2110869082ee152ef
Yury Bolshakov <juriii155@gmail.com>
2026-01-30 22:38:51 +0300

    created kafka config

--- app/core/config/components/__init__.py
+++ app/core/config/components/__init__.py
@@ -1,8 +1,9 @@
 from .db_config import DataBaseConfig
 from .jwt_config import JWTConfig
+from .kafka_config import KafkaConfig
 
 
-class ComponentsConfig(DataBaseConfig, JWTConfig): ...
+class ComponentsConfig(DataBaseConfig, JWTConfig, KafkaConfig): ...
 
 
 __all__ = ["ComponentsConfig"]
--- app/core/server/lifespan.py
+++ app/core/server/lifespan.py
@@ -2,38 +2,23 @@ import logging
 from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.infastructure.database import db_helper
+from faststream.kafka import KafkaBroker
+from app.core.config import settings
 
 logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    """
-    Manage the lifespan of the FastAPI application with asynchronous startup and shutdown events.
+    db_helper = DataBaseHelper(
+        db_url=settings.postgres_connection,
+        echo=True,
+    )
 
-    This async context manager is used to handle setup and teardown logic for the FastAPI
-    application lifecycle. Currently, it supports shutdown operations such as safely
-    disposing of the database connection pool. Startup logic can be added as needed.
-
-    The function yields control back to the application during its runtime. On shutdown,
-    it ensures that the database helper disposes of any active connections or resources.
-
-    Args:
-        app (FastAPI): The FastAPI application instance to which this lifespan is attached.
-
-    Yields:
-        AsyncGenerator[None, None]: A generator that yields control during app runtime.
-
-    Example:
-        app = FastAPI(lifespan=app_lifespan)
-
-    Note:
-        Lines for creating and dropping tables are commented out and can be enabled
-        for development or testing if needed.
-    """
+    broker = KafkaBroker(bootstrap_servers=settings)
 
     await db_helper.create_tables()
+    app.state.pro
     yield
     # await db_helper.drop_tables()
     await db_helper.dispose()
