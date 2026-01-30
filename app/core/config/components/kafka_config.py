aa9e04a3dde74c45be4bf9e173e146b8a5e9ebe8
Yury Bolshakov <juriii155@gmail.com>
2026-01-31 00:03:19 +0300

    created pg admin config + updated kafka params

--- app/core/config/components/kafka_config.py
+++ app/core/config/components/kafka_config.py
@@ -4,14 +4,13 @@ from ..constans import ENV_FILE_PATH
 
 
 class KafkaConfig(BaseSettings):
-    kafka_bootstrap_servers: str = Field(...)
-    kafka_acks: str | int = Field(...)
-    kafka_retries: int = Field(...)
-    kafka_delivery_timeout_ms: int = Field(...)
-    kafka_request_timeout_ms: int = Field(...)
-    kafka_linger_ms: int = Field(...)
-    kafka_enable_idempotence: bool = Field(...)
-    kafka_max_in_flight_requests_per_connection: int = Field(...)
+    kafka_bootstrap_servers: str = Field(default="111")
+    kafka_acks: str | int = Field(default="all")
+    kafka_retries: int = Field(default=10000)
+    kafka_request_timeout_ms: int = Field(default=111)
+    kafka_linger_ms: int = Field(default=111)
+    kafka_enable_idempotence: bool = Field(default=True)
+    kafka_max_in_flight_requests_per_connection: int = Field(default=5)
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,
