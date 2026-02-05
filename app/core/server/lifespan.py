2ee54addffe9738d8c295392313436ce46661e6e
Yury Bolshakov <juriii155@gmail.com>
2026-02-05 20:30:36 +0300

    the singleton storage mechanism has been redesigned

--- app/core/server/lifespan.py
+++ app/core/server/lifespan.py
@@ -1,18 +1,20 @@
-import logging
 from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.infastructure import init_db, init_kafka
-
-logger = logging.getLogger(__name__)
+from app.infastructure.database.db_helper import create_db_helper
+from app.infastructure.kafka.kafka_broker import create_kafka_broker
+from app.api.kafka_demo.kafka_demo_consumer import register_kafka_demo_consumers
 
 
 @asynccontextmanager
 async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    db_helper = await init_db(app)
-    broker = await init_kafka(app)
+    db_helper = create_db_helper()
+    broker = create_kafka_broker()
 
-    # await db_helper.create_tables()
+    app.state.db_helper = db_helper
+    app.state.broker = broker
+    await db_helper.create_tables()
+    register_kafka_demo_consumers(broker=broker)
     await broker.start()
     yield
     # await db_helper.drop_tables()
